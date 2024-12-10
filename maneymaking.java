023dfd67dd508997f28952bdfd38aca563de03d1
Yashwant <anyname6281@gmail.com>
2024-12-10 11:13:08 +0530

    Add files via upload

---
+++ maneymaking.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+int y=sc.nextInt();
+if(0<=x&&y<=10)
+{
+   int c=5000*x+9800*y;
+   System.out.println(c);
+}
+
+}
+}
