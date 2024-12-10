f3e0be3a224c968ed47f625670f302529d9654df
Yashwant <anyname6281@gmail.com>
2024-12-10 11:13:28 +0530

    Add files via upload

---
+++ cricketcup.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+   Scanner sc=new Scanner(System.in);
+   int x=sc.nextInt();
+   if(12<=x&&x<=20)
+   {
+       System.out.println("yes");
+   }
+   else{
+       System.out.println("no");
+   }
+
+
+}
+}
