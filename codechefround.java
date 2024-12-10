b2af183329ac0f390909a16f0770601d5adc706e
Yashwant <anyname6281@gmail.com>
2024-12-10 12:30:45 +0530

    Add files via upload

---
+++ codechefround.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<=3||5<=n)
+		{
+		    System.out.println("no");
+		}
+		else
+		{
+		    System.out.println("yes");
+		}
+
+	}
+}
