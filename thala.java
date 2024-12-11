b103260377ea67018aad80ec398c754b437a36ab
Yashwant <anyname6281@gmail.com>
2024-12-11 09:45:47 +0530

    Add files via upload

---
+++ thala.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+   Scanner sc=new Scanner(System.in);
+   int x=sc.nextInt();
+   if(x<=6 || 8<=x)
+   {
+       System.out.println("SADGE");
+   }
+   else
+   {
+       System.out.println("THALA");
+   }
+
+
+}
+}
