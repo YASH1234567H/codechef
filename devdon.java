afcefc6f0ccaae1a66476421032cba6389c493c6
Yashwant <anyname6281@gmail.com>
2024-12-12 10:33:42 +0530

    Add files via upload

---
+++ devdon.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if(x<=10&& y>=200 &&y<=300)
+		{
+		    System.out.println(x*y);
+		}
+
+	}
+}
