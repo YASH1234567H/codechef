f66eb5b25731dd78b9a2ce0d409582da82fa45f0
Yashwant <anyname6281@gmail.com>
2024-12-14 15:06:33 +0530

    Add files via upload

---
+++ fit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            System.out.println(10*x);
+            
+        }t++;
+    }
+}
