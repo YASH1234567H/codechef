6e755258067279151cc6367df3fc1e56668697ad
Yashwant <anyname6281@gmail.com>
2024-12-15 11:19:13 +0530

    Add files via upload

---
+++ ben_apt.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int a=n*x;
+		int b=m*y;
+		System.out.println(a+b);
+
+	}
+}
