ce9ce079ad3b13f81fce4d2a7511e812a78dd65d
Yashwant <anyname6281@gmail.com>
2024-12-16 13:39:01 +0530

    Add files via upload

---
+++ creds.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		for(int i=0;i<x;i++)
+		{
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    System.out.println(4*a+2*b);
+		    
+		}  
+
+	}
+}
