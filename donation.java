61ed600917d625426196379aa87e6e2472e0f5cd
Yashwant <anyname6281@gmail.com>
2024-12-16 11:04:52 +0530

    Add files via upload

---
+++ donation.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		for(int i=0;i<x;i++)
+		{
+		    int a=sc.nextInt();
+		    int t=sc.nextInt();
+		    System.out.println(t-a);
+		}  
+
+	}
+}
+
