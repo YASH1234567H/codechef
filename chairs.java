6420ac7626065e4d09f91bfd8c2e9f5a81a54d7c
Yashwant <anyname6281@gmail.com>
2024-12-16 11:04:18 +0530

    Add files via upload

---
+++ chairs.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		for(int i=0;i<x;i++)
+		{
+		    int a=sc.nextInt();
+		    int t=sc.nextInt();
+		    if(a<=t)
+		    {
+		        System.out.println("0");
+		    }else{
+		        System.out.println(a-t);
+		    }
+		}  
+
+	}
+}
