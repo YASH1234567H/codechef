1b68de30fdb405521d5e1ab4fd4e8c780ef58cee
Yashwant <anyname6281@gmail.com>
2024-12-17 07:26:26 +0530

    Add files via upload

---
+++ r5s.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a+b>=2000){
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+
+	}
+}
